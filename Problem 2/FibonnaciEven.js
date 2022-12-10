e9ecf403ce4debd0265d7df6a9f721f87be070ef
TheBasementNerd <nerdofthebasement@gmail.com>
2022-12-10 01:51:10 -0600

    Problem solution, I feel this could be improved by a better knowledge of the way that fibonacci numbers function

--- Problem 2/FibonnaciEven.js
+++ Problem 2/FibonnaciEven.js
@@ -1,9 +1,11 @@
 //By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms
 
 var nums = [1, 2];
+var sum = 0;
 
 for (let i=0; i<4000000; i=nums[1]) {
 	nums.push(nums.shift() + nums[0]);
+	sum += (nums[0]%2 == 0) ? nums[0] : 0;
 }
 
-console.log(nums);
+console.log(sum);
