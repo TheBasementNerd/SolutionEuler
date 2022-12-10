8a6cb69974f159f6d3ec04cbf95777311c768467
TheBasementNerd <nerdofthebasement@gmail.com>
2022-12-10 00:09:49 -0600

    First working version, wrong answer but near solution

--- Problem 1/MultiplesUnder1000.js
+++ Problem 1/MultiplesUnder1000.js
@@ -1,1 +1,13 @@
-console.log("Testing 21");
+//Find the sum of all the multiples of 3 or 5 below 1000
+
+function Multiple(inp, bel) {
+	let oup = 0;
+	for (let i=0; i<=bel; i += inp) {
+		oup += i;
+		console.log(i);
+	}
+
+	return oup;
+}
+
+console.log(Multiple(3, 1000) + Multiple(5, 1000));
--- Problem 1/Testing.js
+++
@@ -1,1 +0,0 @@
-console.log("Another test 2");
